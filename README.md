ef7ada3c684b819e7790431a617bc5df49cadbf1
Md. Mahfuzur Rahman <mahfuzak08@gmail.com>
2019-11-07 08:04:37 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Print a div content using Jquery
+	How to use?
+		This is very simple, just include print.js in your page, and call printDiv function by your div id.
---
+++ print.js
@@ -0,0 +1,16 @@
+function printDiv(id) 
+{
+
+  var divToPrint=document.getElementById(id);
+
+  var newWin=window.open('','Print-Window');
+
+  newWin.document.open();
+
+  newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
+
+  newWin.document.close();
+
+  setTimeout(function(){newWin.close();},10);
+
+}
